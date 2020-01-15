79ab2677f440ddb0dd92c0717ccdbd78f7d15ef1
LiangG <gaoliang.leon@gmail.com>
2020-01-14 19:11:47 -0500

    init files

---
+++ precache-manifest.dd3bddecee7d32a90ee13315338d555c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7065d67eae81650e3e447eb1e9b82990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635159aeaca9f0b29eb3",
+    "url": "/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "4e0eaed01b66675b5e3b",
+    "url": "/static/css/main.04f97d47.chunk.css"
+  },
+  {
+    "revision": "635159aeaca9f0b29eb3",
+    "url": "/static/js/2.f84d6f5b.chunk.js"
+  },
+  {
+    "revision": "4e0eaed01b66675b5e3b",
+    "url": "/static/js/main.e7119258.chunk.js"
+  },
+  {
+    "revision": "bb81281f0131da701a37",
+    "url": "/static/js/runtime-main.ccca170d.js"
+  },
+  {
+    "revision": "1e5fb6838887a0990aed9523379f2aba",
+    "url": "/static/media/logo.1e5fb683.png"
+  }
+]);
